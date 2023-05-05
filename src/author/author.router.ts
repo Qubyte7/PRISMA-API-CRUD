b69016437a9686eb0a52f04628c5308d78899152
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-05-05 07:31:40 +0200

    done with all author.service.ts

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -14,6 +14,82 @@ export const authorRouter = express.Router();
            return res.status(500).json(error.message);
     }
 })
+//Get an author
+authorRouter.get("/:id",async(req:Request,res:Response)=>{
+    const id:number = parseInt(req.params.id,10);
+    try{
+        const author = await AuthorService.getAuthor(id)
+        if(author ){
+            return res.status(200).json(author)
+        }else{
+            return res.status(404).json("Author could not be found")
+        }
+    }catch(error:any){
+          return res.status(500).json(error.message);
+    }
+})
+//POST:create an Author
+//Params: first name, lastname
+authorRouter.post("/", body("firstName"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -5,15 +5,15 @@ type Author ={
     id: number;
     firstName:string;
     lastName:string;
-    createdAt: Date;
+    // createdAt: Date;
 }
-export const listAuthors = async ():Promise<Author[]> =>{
+export const listAuthors = async ():Promise<Author[]> =>{//The promise you are seeing is because the function isan async
        return db.author.findMany({
         select:{
             id:true,
             firstName:true,
             lastName:true,
-            createdAt:true
+            // createdAt:true
         }
        })
 }
@@ -22,13 +22,49 @@ export const getAuthor = async (id:number):Promise<Author | null> =>{
     return   db.author.findUnique({
         where:{
             id,//this is the same as id :id,
-        }
+        },
     })
 }
 
-
-
-
+export const createAuthor = async (author: Omit<Author ,"id">):Promise <Author> =>{//omit Omits the id in the Author
+    const {firstName,lastName} = author
+    return db.author.create({
+        data:{
+            firstName,
+            lastName,
+        },select:{
+            id:true,
+            firstName:true,
+            lastName:true,
+        }
+    })
+}
+//UpDATING AN AUTHOR
+export const updateAuthor = async(author : Omit <Author, "id">, id:number):Promise <Author> => {
+    const {firstName,lastName} = author;
+    return db.author.update({
+        where:{
+            id,//remember that is the same as saying id : id,
+        },
+        data:{
+            firstName,//means that firstname = firstname
+            lastName,
+        },
+        select:{
+            id:true,
+            firstName:true,
+            lastName:true,
+        }
+    })
+}
+//Deleting  author
+export const deleteAuthor = async (id:number):Promise<void> => {
+    await db.author.delete({
+        where:{
+            id,
+        }
+    })
+}
 
 
 
